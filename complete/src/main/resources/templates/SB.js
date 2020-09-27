fecfe6f0bd960811511d4ba083eccd0fd19e4d36
loceyashley <locey_ashley@columbusstate.edu>
2020-09-27 17:01:09 +0000

    added

---
+++ complete/src/main/resources/templates/SB.js
@@ -0,0 +1,7 @@
+arrayOfBreeds = [];
+
+//Add breeds to the list 
+function addBreed(){
+    var Breed = document.getElementById('exampleFormControlTextarea1');
+    arrayOfBreeds.push(Breed.value);
+}
